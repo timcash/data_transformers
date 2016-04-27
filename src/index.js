7435f55b780b2d09b6a3a65bdf81220eddc65410
Tim Cash <timcash@gmail.com>
2016-04-27 10:40:45 -0700

    fix

--- src/index.js
+++ src/index.js
@@ -36,14 +36,25 @@ const set = (k, v, hmap) => {
     hmap[k] = v
     return hmap
 }
-const objectSwapper = (from, to) => R.pipe(R.pick(from), R.values, R.zipObj(to))
-const extractKey = (agg, n) => set(n[key], n, agg)
-const arrayToKeyed = (key, arr) => R.reduce(extractKey, {}, arr)
+
+const objectSwapper = (from, to) => {
+    let pic = R.pick(from)
+    let val = R.values
+    let zip = R.zipObj(to)
+    return R.pipe(pic, R.values, zip)
+}
+
+const arrayToKeyed = (key, arr) => {
+    extractKey = (collector, n) => set(n[key], n, collector)
+    return R.reduce(extractKey, {}, arr)
+}
+
 const swapAndKey = (from, to, input_obj) => {
   const swapper = objectSwapper(from, to)
-  const swapped = R.map(swapper, input)
+  const swapped = R.map(swapper, input_obj)
   return arrayToKeyed('feature', swapped)
 }
+
 const k1 = swapAndKey(
   ['id','isEnabledForUser','isAvailableForOptIn'],
   ['feature', 'enabled', 'optin'],
